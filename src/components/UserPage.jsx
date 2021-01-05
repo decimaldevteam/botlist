d361e874e9c4d8790a6dba6c189196bc38bcd8ee
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2021-01-05 11:23:31 +0530

    Delete UserPage.jsx

--- src/components/UserPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function UserPage({ user }){
-    return <div className="userpage">
-        <img src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?size=2048`} alt="avatar"/>
-        <h1>{user.username}  <font style={{ opacity: 0.7 }}># {user.discriminator}</font></h1>
-    </div>;
-}
