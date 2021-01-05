8e86d949ca2cec1e65f48d6b5f0f19961b6c268a
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2021-01-05 11:22:36 +0530

    Delete Badge.jsx

--- src/components/Badge.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Badge({ title, value, color }){
-    return <span className="badge">
-        <span className="badge-content">{title}</span>
-        <span className="badge-cover" style={{ backgroundColor: color }}>{value}</span>
-    </span>
-}
